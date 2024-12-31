1e6ad17e37ff562824a0442c3a51bd64081fc4eb
Irene Diao <irened22@tamu.edu>
2024-12-31 11:53:08 +0800

    Add new movies to seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
   {:title => 'Knives Out', :rating => 'PG-13',
-    :release_date => '15-Nov-2019'}
+    :release_date => '15-Nov-2019'},
   {:title => 'Avengers', :rating => 'PG-13',
-    :release_date => '20-Jun-2020'}
+    :release_date => '20-Jun-2020'},
   {:title => 'Despicable Me', :rating => 'PG',
     :release_date => '10-May-2018'}
 ]
